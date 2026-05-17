b6398c3bb895a77ca91c3156174cfa55d18cd8e3
Avi Lupovitch <lupovita@post.bgu.ac.il>
2026-05-17 16:17:41 +0300

    Added megahint highlight functionality, and started splitting files.

---
+++ js/globals.js
@@ -0,0 +1,55 @@
+'use strict'
+
+const LEVELS = {
+    BEGGINER: {SIZE: 4, MINES: 2},
+    MEDIUM: {SIZE: 8, MINES: 14},
+    EXPERT: {SIZE: 12, MINES: 32}
+};
+
+const CLASS_BEGGINER = 'btn-begginer', CLASS_MEDIUM = 'btn-medium', CLASS_EXPERT = 'btn-expert';
+
+const SECOND = 1000;
+const TIMER_INTERVAL_TIMEOUT = 500;
+const MINE_CLICK_TIMEOUT = 500;
+const HINT_CLICK_TIMEOUT = 1500;
+const SAFE_CLICK_TIMEOUT = 1500;
+const MEGA_HINT_CLICK_TIMEOUT = 2000;
+const LIVES_AMOUNT = 3;
+const HINTS_AMOUNT = 3;
+const LEVELS_AMOUNT = 3;
+const SAFE_CLICKS_AMOUNT = 3;
+const EXTERMINATE_MINES_AMOUNT = 3;
+
+const MINE_HTML = getImgHTML('mine', 'png');
+const FLAG_HTML = getImgHTML('flag', 'png');
+const FACE_CLICK_HTML = getImgHTML('face-click', 'png', 'onInit()');
+const FACE_LOSE_HTML = getImgHTML('face-lose', 'png', 'onInit()');
+const FACE_SMILE_HTML = getImgHTML('face-smile', 'png', 'onInit()');
+const FACE_WIN_HTML = getImgHTML('face-win', 'png', 'onInit()');
+const LIFE_HTML = getImgHTML('life', 'gif');
+const CLOCK_HTML = getImgHTML('clock', 'png');
+const EXPLOSION_HTML = getImgHTML('explosion', 'gif');
+const HINT_ON_HTML = getImgHTML('hint-on', 'png', 'onHintClick(this)');
+const HINT_OFF_HTML = getImgHTML('hint-off', 'png', 'onHintClick(this)');
+const HINT_ON_SRC = 'images/hint-on.png';
+const HINT_OFF_SRC = 'images/hint-off.png';
+const DARK_MODE_SRC = 'images/dark-mode.png';
+const LIGHT_MODE_SRC = 'images/light-mode.png';
+
+var gStartTime;
+var gTimerIntervalId;
+var gLevel;
+var gGame;
+var gBoard;
+var gElCellDownClicked;
+var gIsHintClicked;
+var gHintTimeoutId;
+var gSafeClickTimeoutid;
+var gGameMoves;
+var gIsManualModeOn;
+var gMegaHintMode;
+
+function getImgHTML(imgName, fileType, onClickFunc=null) {
+    const onClickHTML = onClickFunc ? `onclick="${onClickFunc}"` : ''
+    return `<img src="images/${imgName}.${fileType}" ${onClickHTML}>`;
+}
--- js/main.js
+++ js/main.js
@@ -1,59 +1,10 @@
 'use strict'
 
-const LEVELS = {
-    BEGGINER: {SIZE: 4, MINES: 2},
-    MEDIUM: {SIZE: 8, MINES: 14},
-    EXPERT: {SIZE: 12, MINES: 32}
-};
-
-const CLASS_BEGGINER = 'btn-begginer', CLASS_MEDIUM = 'btn-medium', CLASS_EXPERT = 'btn-expert'
-
-const SECOND = 1000;
-const TIMER_INTERVAL_TIMEOUT = 500;
-const MINE_CLICK_TIMEOUT = 500;
-const HINT_CLICK_TIMEOUT = 1500;
-const SAFE_CLICK_TIMEOUT = 1500;
-const MEGA_HINT_CLICK_TIMEOUT = 2000;
-const LIVES_AMOUNT = 3;
-const HINTS_AMOUNT = 3;
-const LEVELS_AMOUNT = 3;
-const SAFE_CLICKS_AMOUNT = 3;
-const EXTERMINATE_MINES_AMOUNT = 3;
-
-const MINE_HTML = getImgHTML('mine', 'png');
-const FLAG_HTML = getImgHTML('flag', 'png');
-const FACE_CLICK_HTML = getImgHTML('face-click', 'png', 'onInit()');
-const FACE_LOSE_HTML = getImgHTML('face-lose', 'png', 'onInit()');
-const FACE_SMILE_HTML = getImgHTML('face-smile', 'png', 'onInit()');
-const FACE_WIN_HTML = getImgHTML('face-win', 'png', 'onInit()');
-const LIFE_HTML = getImgHTML('life', 'gif');
-const CLOCK_HTML = getImgHTML('clock', 'png');
-const EXPLOSION_HTML = getImgHTML('explosion', 'gif');
-const HINT_ON_HTML = getImgHTML('hint-on', 'png', 'onHintClick(this)');
-const HINT_OFF_HTML = getImgHTML('hint-off', 'png', 'onHintClick(this)');
-const HINT_ON_SRC = 'images/hint-on.png';
-const HINT_OFF_SRC = 'images/hint-off.png';
-const DARK_MODE_SRC = 'images/dark-mode.png'
-const LIGHT_MODE_SRC = 'images/light-mode.png'
-
-var gStartTime;
-var gTimerIntervalId;
-var gLevel;
-var gGame;
-var gBoard;
-var gElCellDownClicked;
-var gIsHintClicked;
-var gHintTimeoutId;
-var gSafeClickTimeoutid;
-var gGameMoves;
-var gIsManualModeOn;
-var gMegaHintMode;
-
 // TODO: split the js code into organized files: main.js, board.js, globals.js, button-clicks.js, helper-functions.js, 
 //       buttons-active-state.js
 // TODO: split css code into files: main.css, board.css, btns-special.css, header-footer.css, best-scores.css, 
 //       face-and-data.css, dark-mode.css, lives-and-hints.css.
-// TODO: megaHintMode: when gMegaHintMode is on, when gMegaHintMode.locations.length > 0: add '.highlight' class to all the cells between the cell being hovered to the cell saved in locations in the function onCellHovered(this).
+
 function onInit() {
     gStartTime = 0;
     gIsHintClicked = false;
@@ -216,6 +167,7 @@ function renderBoard() {
             strHTML += `\t<td 
                             class="${className}" 
                             data-i="${i}" data-j="${j}"
+                            onpointerenter="onCellEntered(${i}, ${j})"
                             onpointerdown="onCellDownClicked(this, ${i}, ${j}, event)"
                             onclick="onCellClicked(this, ${i}, ${j})"
                             oncontextmenu="onCellMarked(this, ${i}, ${j}); return false;" >\n
@@ -370,6 +322,31 @@ function onCellClicked(elCell, i, j) {
     checkGameFinished(elCell, clickedCell, clickedCell.isMine);
 }
 
+function onCellEntered(i, j) {
+    if (gMegaHintMode.locations.length > 0) {
+        // unhighlight all cells before highlighting the current area:
+        clearHighlightedCells();
+        
+        // highlight the area between the first click and the current cell hovered:
+        const location1 = gMegaHintMode.locations[0], location2 = {i, j};
+        const highlightFunc = (cell, location) => getCellElement(location).classList.add('highlight');
+        manipulateAreaBetweenTwoLocations(gBoard, location1, location2, highlightFunc);
+    }
+}
+
+function clearHighlightedCells() {
+    const unhighlightFunc = (cell, location) => getCellElement(location).classList.remove('highlight');
+    forEach(gBoard, unhighlightFunc);
+}
+
+function manipulateAreaBetweenTwoLocations(mat, location1, location2, func) {
+        const startRow = Math.min(location1.i, location2.i);
+        const endRow = Math.max(location1.i, location2.i);
+        const startCol = Math.min(location1.j, location2.j);
+        const endCol = Math.max(location1.j, location2.j);
+        forEach(mat, func, startRow, endRow, startCol, endCol);
+}
+
 function setMine(elCell, clickedCell) {
     clickedCell.isMine = !clickedCell.isMine;
     gGame.minesCount += clickedCell.isMine ? 1 : -1;
@@ -383,23 +360,17 @@ function handleMegaHint(clickedCellLocation) {
     gMegaHintMode.locations.push(clickedCellLocation);
     if (gMegaHintMode.locations.length < 2) return;
 
-    const location1 = gMegaHintMode.locations[0], location2 = gMegaHintMode.locations[1];
-    const startRow = Math.min(location1.i, location2.i);
-    const endRow = Math.max(location1.i, location2.i);
-    const startCol = Math.min(location1.j, location2.j);
-    const endCol = Math.max(location1.j, location2.j);
-
     updateGameMoves(0);
 
+    const location1 = gMegaHintMode.locations[0], location2 = gMegaHintMode.locations[1];
     const revealFunc = (cell, location) => revealCell(getCellElement(location), cell);
-    forEach(gBoard, revealFunc, startRow, endRow, startCol, endCol);
+
+    manipulateAreaBetweenTwoLocations(gBoard, location1, location2, revealFunc);
 
     setTimeout(onUndoClick, MEGA_HINT_CLICK_TIMEOUT);
 
-    gMegaHintMode.isOn = false;
+    resetMegaHint();
     gMegaHintMode.isUsed = true;
-    const elBtn = document.querySelector('.btn-mega-hint');
-    elBtn.classList.toggle('mega-hint');
 }
 
 function onCellMarked(elCell, i, j) {
@@ -492,6 +463,10 @@ function onManualModeClick(elBtn) {
 
 function onMegaHintClick(elBtn) {
     if (!gGame.isOn || gMegaHintMode.isUsed) return;
+    if (gMegaHintMode.isOn) {
+        gMegaHintMode.locations = [];
+        clearHighlightedCells();
+    }
     gMegaHintMode.isOn = !gMegaHintMode.isOn;
     elBtn.classList.toggle('mega-hint');
 }
@@ -639,11 +614,6 @@ function isGameWon() {
 function changeFace(faceHTML) {
     const elFace = document.querySelector('.face');
     elFace.innerHTML = faceHTML;
-}
-
-function getImgHTML(imgName, fileType, onClickFunc=null) {
-    const onClickHTML = onClickFunc ? `onclick="${onClickFunc}"` : ''
-    return `<img src="images/${imgName}.${fileType}" ${onClickHTML}>`;
 }
 
 function handleMineClick(elCell, clickedCell) {
